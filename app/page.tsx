978e9d40465ac5adfc92c1627fb5dce527d5dbc6
Steinar Karlsson <steinarkarlsson@gmail.com>
2024-10-10 21:25:50 +1300

    Add Authentication with Next-Auth

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,11 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Dashboard',
+};
 
 export default function Page() {
   return (
