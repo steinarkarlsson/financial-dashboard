f6b73ab33bfd211b3057bf1175d7e25d13441ccb
Steinar Karlsson <steinarkarlsson@gmail.com>
2024-10-10 13:55:11 +1300

    Add form validation

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,9 +1,8 @@
-import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
-import { lusitana } from '@/app/ui/fonts';
-import { Suspense }  from 'react';
-import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardSkeleton } from '@/app/ui/skeletons';
+import {lusitana} from '@/app/ui/fonts';
+import {Suspense} from 'react';
+import {CardSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton} from '@/app/ui/skeletons';
 import CardWrapper from '@/app/ui/dashboard/cards';
 
 export default async function Page() {
@@ -14,15 +13,15 @@ export default async function Page() {
                 Dashboard
             </h1>
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-                <Suspense fallback={<CardSkeleton />}>
-                    <CardWrapper />
+                <Suspense fallback={<CardSkeleton/>}>
+                    <CardWrapper/>
                 </Suspense>
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-                <Suspense fallback={<RevenueChartSkeleton />}>
-                <RevenueChart />
+                <Suspense fallback={<RevenueChartSkeleton/>}>
+                    <RevenueChart/>
                 </Suspense>
-                <Suspense fallback={<LatestInvoicesSkeleton />}>
+                <Suspense fallback={<LatestInvoicesSkeleton/>}>
                     <LatestInvoices/>
                 </Suspense>
             </div>
